1ac76091de0fd16bca95fe90f57d9fb8bd0dcc3f
yangxiaodong <yangxiaodong@qianmi.com>
2018-09-21 15:55:50 +0800

    add: 新文章 预告..

--- manager/src/components/Post/PostCommentsGit.js
+++ manager/src/components/Post/PostCommentsGit.js
@@ -1,47 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import injectSheet from "react-jss";
-import FacebookProvider, { Comments } from "react-facebook";
 require("core-js/fn/array/find");
-import 'gitment/style/default.css'
-
-import config from "../../../content/meta/config";
-
-const styles = theme => ({
-  postComments: {
-    margin: "3em 0 0",
-    padding: "3em 0 0",
-    borderTop: "1px solid #ddd"
-  }
-});
-
-// const PostComments = props => {
-//   const { classes, slug, facebook } = props;
-//
-//   const gitment = new Gitment({
-//     owner: 'creasy2010',
-//     repo: 'https://github.com/creasy2010/creasy2010.github.io',
-//     oauth: {
-//       client_id: '1816b072b2ee81711445',
-//       client_secret: '4e5dc1d0b197493c0871c14f35860f7c9d6cd317',
-//     },
-//   })
-//
-//
-//   // gitment.render('comments')
-//
-//   return (
-//     <div id="post-comments" className={classes.postComments}>
-//       <FacebookProvider appId={facebook}>
-//         <Comments
-//           href={`${config.siteUrl}${slug}`}
-//           width="100%"
-//           colorScheme={props.theme.main.colors.fbCommentsColorscheme}
-//         />
-//       </FacebookProvider>
-//     </div>
-//   );
-// };
 
 
 class PostComments extends React.Component{
@@ -78,4 +37,4 @@ PostComments.propTypes = {
   facebook: PropTypes.object.isRequired
 };
 
-export default injectSheet(styles)(PostComments);
+export default PostComments;
