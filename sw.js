17f6db98ee6fe6d8b653adc32a72335c98be9572
yangxiaodong <yangxiaodong@qianmi.com>
2019-01-29 13:41:57 +0800

    auto push at 2019-01-29 13:41:52.395317

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/app-9e689969c24be7015bf7.js","1726673f22ff4f8fd16b49902b94e4ed"],["/commons-c4d8e44a0ee665e0dfe4.js","40c8f86c6af6d67c59a2e4b8235bb39a"],["/index.html","67e8d43dfbbeaefeec34e4436ae150ca"],["/manifest.webmanifest","5404e435c62ce8724808d0950094372f"],["/offline-plugin-app-shell-fallback/index.html","e7938a5d195e9f5febd666ce359869b4"]];
+var precacheConfig = [["/app-086c3cdafc2f792b3d1d.js","728a275aa3e5221e11574af74bda8c13"],["/app-0c18d530acce3119cc60.js","3f8b199fb51605e6a61b86376e7ebf95"],["/app-9e689969c24be7015bf7.js","1726673f22ff4f8fd16b49902b94e4ed"],["/commons-c4d8e44a0ee665e0dfe4.js","40c8f86c6af6d67c59a2e4b8235bb39a"],["/index.html","67e8d43dfbbeaefeec34e4436ae150ca"],["/manifest.webmanifest","5404e435c62ce8724808d0950094372f"],["/offline-plugin-app-shell-fallback/index.html","e7938a5d195e9f5febd666ce359869b4"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 
